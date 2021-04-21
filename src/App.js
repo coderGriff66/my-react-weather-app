8e64661a7d372163b7754e76bf1a94e974a2b961
coderGriff66 <77123924+coderGriff66@users.noreply.github.com>
2021-04-20 20:17:17 -0400

    Published to netlify, Added footer with GitHub link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project coded by Jennifer Griffin and is 
+      <a href="https://github.com/coderGriff66/my-react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
