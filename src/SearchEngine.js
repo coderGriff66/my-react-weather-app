216382efb562900781ecb122e120e7e27b59b2e1
coderGriff66 <77123924+coderGriff66@users.noreply.github.com>
2021-04-21 17:22:37 -0400

    Fixed form & buttons on SearchEngine

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -5,15 +5,15 @@ export default function SearchEngine() {
     <div>
     <form className="Search">
           <div className="row">
-            <div className="col-7">
-              <input type="Search" placeholder="Enter City Name" autofocus="on" autocomplete="off"/>
+            <div className="col-6">
+              <input type="Search" placeholder="Enter City Name" className="form-control" autofocus="on" autocomplete="off"/>
+            </div>
                <div className="col-3">
                 <input type="submit" value="Search" className="btn btn-success"/>
-                  <div className="col-2">
+               </div>
+                  <div className="col-3">
                     <button className="CurrentButton">CURRENT</button>
                   </div>
-               </div>
-            </div>
           </div>
     </form>
       <div>
