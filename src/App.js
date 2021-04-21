934b533e346cb589cf95df4564c0e065fdf04880
coderGriff66 <77123924+coderGriff66@users.noreply.github.com>
2021-04-20 20:43:04 -0400

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project coded by Jennifer Griffin and is 
       <a href="https://github.com/coderGriff66/my-react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
       </footer>
+      </div>
     </div>
   );
 }
