bd0edd632c9040a0f2b4bc0f6318d5b94a496539
coderGriff66 <77123924+coderGriff66@users.noreply.github.com>
2021-04-20 21:27:26 -0400

    Added the WeatherConditions component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import WeatherConditions from "./WeatherConditions";
 
 import './App.css';
 
@@ -7,6 +8,7 @@ export default function App() {
     <div className="App">
       <div className="container">
       <h1>Weather App</h1>
+      <WeatherConditions />
       <footer>
         This project coded by Jennifer Griffin and is 
       <a href="https://github.com/coderGriff66/my-react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
---
+++ src/WeatherConditions.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function WeatherConditions() {
+  return (
+    <div className="WeatherConditions"> Hello from Weather</div>
+  )
+
+}
