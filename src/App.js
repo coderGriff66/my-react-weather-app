3c668c4558492c1777bb836a4323ee373dc8fd27
coderGriff66 <77123924+coderGriff66@users.noreply.github.com>
2021-04-22 19:48:39 -0400

    Added axios, started implementing some css, functions for WeatherConditions

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import React from "react";
 import WeatherConditions from "./WeatherConditions";
 import HeaderDate from "./HeaderDate";
 import SearchEngine from "./SearchEngine";
-import './App.css';
+
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-       <HeaderDate /> 
-       <SearchEngine />
+        <div className="WeatherApp">
+          <HeaderDate /> 
+          <SearchEngine />
        
         
-        <WeatherConditions />
+          <WeatherConditions />
 
-      <footer>
-        This project coded by Jennifer Griffin and is 
-      <a href="https://github.com/coderGriff66/my-react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
-      </footer>
+          <footer>
+            This project coded by Jennifer Griffin and is 
+            <a href="https://github.com/coderGriff66/my-react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
+          </footer>
+        </div>
       </div>
     </div>
   );
--- src/WeatherConditions.js
+++ src/WeatherConditions.js
@@ -1,37 +1,64 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 import "./WeatherConditions.css";
 
 export default function WeatherConditions() {
+  const [weatherData, setWeatherData] = useState({ ready: false});
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      feelsLike: response.data.main.feels_like,
+      barometer: response.data.main.pressure,
+      humidity: response.data.main.humidity,
+      winds: response.data.wind.speed,
+      minTemp: response.data.weather.min_temp,
+      city: response.data.name
+
+    });
+    
+    setReady(true);
+  }
+
+   if (ready) {
   return (
     <div className="WeatherConditions">
       <div className="row">
         <div className="col-7">
           <div className="Card Leftside">
-          <img src="/" alt="Clear_Sky"/>
-          10°C
+            <h2 classNmae="text-capitalize">{weatherData.description}</h2>
+            <div className="clearfix">
+              <img src="/" alt="Clear_Sky" className="float-left"/>
+                <span ClassName="temperature">{Math.round(weatherhData.temperature)}</span>
+                <span className="unit">°C</span>
+            </div>
           </div>
         </div>
           <div className="col-5">
             <div className="Card Rightside">
              <ul className="Conditions">
-              <li>Feels Like: 30°</li>
-              <li>Barometer: 29.85</li>
-              <li>Humidity: 45%</li>
-              <li>Wind: 7 km/h</li>
-              <li>Day's Low Temp: 25°</li>
+              <li>Feels Like: {weatherData.feels_like}°</li>
+              <li>Barometer: {weatherData.baromrter}</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+              <li>Day's Low Temp: {weatherData.min_temp}°</li>
               </ul>
             </div>
           </div>
       </div>
     </div>
 
-     
+  );
 
-      
-      
-      
-      
-  )
+} else {
+  const apiKey="06e5d3dda0232566f39a1df37e2d5cdd";
+  let city="Detroit";
+  let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
+  axios.get(apiUrl). then(handleResponse);
+
+    return "Loading";
+  }
 }
